064ec552bdc04d9cdfa6fe859c42885111a58c50
“Aakash <akashisvaidya@gmail.com>
2022-11-06 13:50:58 +1100

    https fixed

--- src/components/utilities/axiosHelpers.js
+++ src/components/utilities/axiosHelpers.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchData = (str) => {
   //   const apiEp = "http://www.omdbapi.com/?apikey=5e445267&t=" + str;
-  const apiEp = `http://www.omdbapi.com/?apikey=5e445267&t=${str}`;
+  const apiEp = `https://www.omdbapi.com/?apikey=5e445267&t=${str}`;
   //   console.log(str);
   const response = axios(apiEp);
   return response;
